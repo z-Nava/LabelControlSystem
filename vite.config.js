ed114b19e47868acb0c9fd262dc3a1ec5837191d
z-Nava <62721391+z-Nava@users.noreply.github.com>
2026-07-07 12:05:04 -0600

    adding logo to the login

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/img/favicon.png'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/img/favicon.png', 'resources/img/logoIndex.png'],
             assets: ['resources/img/**'],
             refresh: true,
         }),
